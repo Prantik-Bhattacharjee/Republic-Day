811f1bfbf39568400ccabb6e70dc5ab6ed536787
Prantik-Bhattacharjee <102134319+Prantik-Bhattacharjee@users.noreply.github.com>
2022-03-31 22:04:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+document.querySelector('.prantik').addEventListener('click',function() {
+  var audio= new Audio('sound/prantikeng.ogg');
+  audio.play();
+});
+
+document.querySelector('.su').addEventListener('click',function() {
+  var audio2= new Audio('sound/bengali.ogg');
+  audio2.play();
+});
+
+document.querySelector('.assam').addEventListener('click',function() {
+  var audio2= new Audio('sound/assam.ogg');
+  audio2.play();
+});
+
+document.querySelector('.marathi').addEventListener('click',function() {
+  var audio2= new Audio('sound/marathi.ogg');
+  audio2.play();
+});
+document.querySelector('.tamil').addEventListener('click',function() {
+  var audio2= new Audio('sound/telugu.ogg');
+  audio2.play();
+});
+document.querySelector('.hindi').addEventListener('click',function() {
+  var audio2= new Audio('sound/hindi.ogg');
+  audio2.play();
+});
